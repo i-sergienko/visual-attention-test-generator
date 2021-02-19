32bf3d9d582cfb80bc4df99e13e76c4c77655ac4
Ivan <ivansergienko3211@gmail.com>
2021-02-19 19:15:30 +0100

    License, authors

--- main/src/sergienko/ivan/psych/test/PsychTestGenerator.kt
+++ main/src/sergienko/ivan/psych/test/PsychTestGenerator.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Ivan Sergienko, Alexey Sergienko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package sergienko.ivan.psych.test
 
 import sergienko.ivan.psych.test.generator.cmsToPixel
@@ -10,8 +26,13 @@ import java.nio.file.Path
 import java.util.*
 import javax.imageio.ImageIO
 
-const val DPI = 300//72
+const val DPI = 300
 
+/**
+ * Test image generator entry point.
+ *
+ * @author Ivan Sergienko
+ * */
 fun main(args: Array<String>) {
     val gridWidth = 24
     val gridHeight = 21
@@ -22,24 +43,24 @@ fun main(args: Array<String>) {
     val directory = generateSubdirectory()
 
     val imageColored =
-        symbolTable.toImage(
-            gridWidth = gridWidth,
-            gridHeight = gridHeight,
-            cellSize = cellSize,
-            drawGrid = true,
-            coloredA = true
-        )
+            symbolTable.toImage(
+                    gridWidth = gridWidth,
+                    gridHeight = gridHeight,
+                    cellSize = cellSize,
+                    drawGrid = true,
+                    coloredA = true
+            )
     imageColored.save("${directory.toAbsolutePath()}/colored.jpg")
 
     val imageNotColored =
-        symbolTable.toImage(
-            gridWidth = gridWidth,
-            gridHeight = gridHeight,
-            cellSize = cellSize
-        )
+            symbolTable.toImage(
+                    gridWidth = gridWidth,
+                    gridHeight = gridHeight,
+                    cellSize = cellSize
+            )
 
     imageNotColored.toPDF(
-        path = "${directory.toAbsolutePath()}/print.pdf"
+            path = "${directory.toAbsolutePath()}/print.pdf"
     )
     imageNotColored.save("${directory.toAbsolutePath()}/noColor.jpg")
 
--- main/src/sergienko/ivan/psych/test/generator/Measurements.kt
+++ main/src/sergienko/ivan/psych/test/generator/Measurements.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Ivan Sergienko, Alexey Sergienko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package sergienko.ivan.psych.test.generator
 
 // The number of CMs per Inch
@@ -11,7 +27,9 @@ val INCH_PER_MM = 25.45
  * Converts the given pixels to cm's based on the supplied DPI
  * @param pixels
  * @param dpi
- * @return
+ * @return value in cms
+ *
+ * @author Ivan Sergienko
  */
 fun pixelsToCms(pixels: Double, dpi: Double): Double {
     return inchesToCms(pixels / dpi)
@@ -21,7 +39,9 @@ fun pixelsToCms(pixels: Double, dpi: Double): Double {
  * Converts the given cm's to pixels based on the supplied DPI
  * @param cms
  * @param dpi
- * @return
+ * @return value in pixels
+ *
+ * @author Ivan Sergienko
  */
 fun cmsToPixel(cms: Double, dpi: Double): Double {
     return cmToInches(cms) * dpi
@@ -30,7 +50,9 @@ fun cmsToPixel(cms: Double, dpi: Double): Double {
 /**
  * Converts the given cm's to inches
  * @param cms
- * @return
+ * @return value in inches
+ *
+ * @author Ivan Sergienko
  */
 fun cmToInches(cms: Double): Double {
     return cms * CM_PER_INCH
@@ -39,7 +61,9 @@ fun cmToInches(cms: Double): Double {
 /**
  * Converts the given inches to cm's
  * @param inch
- * @return
+ * @return value in cms
+ *
+ * @author Ivan Sergienko
  */
 fun inchesToCms(inch: Double): Double {
     return inch * INCH_PER_CM
